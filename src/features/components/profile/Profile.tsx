a2edf953644d311fd516efd1396d84678f12136c
Yauhen Davidovich <davidovich336@gmail.com>
2021-09-10 17:35:25 +0200

    predfinish

--- src/features/components/profile/Profile.tsx
+++ src/features/components/profile/Profile.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {H3} from "../../../main/ui/commonStyle";
+import {useSelector} from "react-redux";
+import {AppStateType} from "../../../main/bll/store";
+import {Redirect, Route, useHistory} from "react-router-dom";
+import ProfileInfo from "./ProfileInfo";
 
 const Profile = () => {
     const isLoggedIn = useSelector<AppStateType, boolean>(state => state.login.isAuth);
     const history = useHistory();
     return (
-        <div>
-            <H3>Profile</H3>
-        </div>
+        <>
+            {isLoggedIn ? <ProfileInfo/> : <div>You are not author</div>}
+        </>
     )
 }
 
